41546c1d7a0fbac00d6fbd2a9ba9c38202571706
erickyvand <erickyvand@gmail.com>
2020-05-24 16:59:46 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # simple-registration
+
+This is an application created using HTML, CSS and Vanilla Javascript. It is responsive and supports all types of devices.
+
+It has 3 fields to fill:
+- First Name
+- Last Name
+- Occupation
+
+All 3 fields are required, if you submit without filling any field the application will throw an error message.
+
+Once you have successfully submitted the form, the results will be shown in the table. Table includes another colum where you can delete a record.
+
+## Link to access the application
+- [Simple-registration](https://erickyvand.github.io/simple-registration/)
---
+++ dist/script.js
@@ -0,0 +1,40 @@
+const btn = document.querySelector('#btn');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const occupation = document.querySelector('#occupation');
+const table = document.querySelector('.table');
+const message = document.querySelector('#message');
+
+btn.addEventListener('click', e => {
+  e.preventDefault();
+
+  if (!firstName.value || !lastName.value || !occupation.value) {
+    message.classList.add('error-message');
+    message.innerHTML = 'All fields are required';
+    setTimeout(() => {
+      message.remove();
+      location.reload();
+    }, 6000)
+  } else {
+    const row = table.insertRow(1)
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
+    const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
+  
+    cell1.innerHTML = firstName.value,
+    cell2.innerHTML = lastName.value,
+    cell3.innerHTML = occupation.value;
+    cell4.innerHTML = 'Delete';
+
+    cell4.setAttribute('id', 'remove');
+    document.querySelector('#remove').classList.add('delete')
+    document.querySelector('#remove').addEventListener('click', () => {
+      row.remove();
+    })
+  
+    firstName.value = '',
+    lastName.value = '',
+    occupation.value = ''
+  }
+})
