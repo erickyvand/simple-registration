5c44acf64ec31da5f7fbf166c29b065007712f96
erickyvand <erickyvand@gmail.com>
2020-05-25 09:58:53 +0200

    Updates

--- script.js
+++ script.js
@@ -13,14 +13,22 @@ btn.addEventListener('click', e => {
     message.innerHTML = 'All fields are required';
     setTimeout(() => message.remove(), 6000)
   } else {
-    const row = table.insertRow()
+    const row = table.insertRow(1)
     const cell1 = row.insertCell(0);
     const cell2 = row.insertCell(1);
     const cell3 = row.insertCell(2);
+    const cell4 = row.insertCell(3);
   
     cell1.innerHTML = firstName.value,
     cell2.innerHTML = lastName.value,
     cell3.innerHTML = occupation.value;
+    cell4.innerHTML = 'Delete';
+
+    cell4.setAttribute('id', 'remove');
+    document.querySelector('#remove').classList.add('delete')
+    document.querySelector('#remove').addEventListener('click', () => {
+      row.remove();
+    })
   
     firstName.value = '',
     lastName.value = '',
